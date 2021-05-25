0304cbfa408bc2e2747f06bb178a33bade791022
Mihai Bălăceanu <gizm0bill@gmail.com>
2021-05-25 20:18:05 +0300

    testing pipeline

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gzm
+![tests](https://github.com/gizm0bill/gzm/actions/workflows/test.yml/badge.svg)
--- libs/ng-rest-client/karma.conf.js
+++ libs/ng-rest-client/karma.conf.js
@@ -13,7 +13,10 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        random: false // disable the random running order
+      }
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
@@ -25,7 +28,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    browsers: ['Chrome', 'ChromeHeadlessCustom'],
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
   });
 };
