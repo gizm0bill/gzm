f78f937d9e95983b8504209062f30b71e3db8488
Mihai Bălăceanu <gizm0bill@gmail.com>
2025-02-17 21:50:54 +0200

    fix(deps): reflect-metadata

--- demos/ng-rest-client/lib/index.ts
+++ demos/ng-rest-client/lib/index.ts
@@ -1,7 +1,6 @@
-/*
- * Public API Surface of ng-rest-client-demo-lib
+/**
+ * TODO:
  */
-
 export * from './api.service';
 export * from './lib/ng-rest-client-demo-lib.component';
 export * from './lib/ng-rest-client-demo-lib.module';
